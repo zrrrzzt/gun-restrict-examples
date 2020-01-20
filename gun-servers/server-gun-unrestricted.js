ede969d238dfe53c29d0333095fead4518a63867
Mark Nadal <mark@accelsor.com>
2020-01-20 09:56:14 -0800

    Security patch, see https://github.com/amark/gun/issues/880

--- gun-servers/server-gun-unrestricted.js
+++ gun-servers/server-gun-unrestricted.js
@@ -1,19 +1,7 @@
 const port = 8000
 const Gun = require('gun')
 
-const server = require('http').createServer((req, res) => {
-  // filters gun requests!
-  if (Gun.serve(req, res)) {
-    return
-  }
-  require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error', function () {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(require('fs')
-      .readFileSync(require('path')
-        .join(__dirname, 'index.html')
-      ))
-  }).pipe(res)
-})
+const server = require('http').createServer(Gun.serve(__dirname));
 
 const gun = Gun({
   file: 'data.json',
